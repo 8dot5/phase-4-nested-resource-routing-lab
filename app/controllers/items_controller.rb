9b72b5e1c65ad29996f49977d75a4c01e2e51314
Chun Yi <8.5by11@gmail.com>
2022-03-17 14:32:36 -0400

    commit tree d522692bc78d64047ef427985a0358cd02ee4eaf

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  # GET /users/:user_id/items
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  #GET /users/:user_id/items/:id
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  # POST /users/:user_id/items
+  def create
+    item = Item.create!(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def render_not_found_response(error)
+    render json: { error: "#{error.model} Not Found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
